984a15e2c155b28e76853c5a5deb443252277939
Rakesh Tripathi <rakeshtripathi408@gmail.com>
2022-08-21 13:08:49 +0530

    TweetBox component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,6 @@
 import { RefreshIcon } from "@heroicons/react/outline";
 import React from "react";
+import TweetBox from "./TweetBox";
 
 const Feed: React.FC = () => {
   return (
@@ -7,6 +8,10 @@ const Feed: React.FC = () => {
       <div className="flex items-center justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
         <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+      </div>
+
+      <div>
+        <TweetBox />
       </div>
     </div>
   );
